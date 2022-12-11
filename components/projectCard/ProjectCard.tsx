67abab57ef7bc563321fb604d350b41114323521
yarinmatmoni <yarinmatmoni@gmail.com>
2022-12-12 00:08:44 +0200

    add href to projectCard component

--- components/projectCard/ProjectCard.tsx
+++ components/projectCard/ProjectCard.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import styles from './ProjectCard.module.scss';
 
-function ProjectCard({ name, src, alt, skills }: { name: string, src: string, alt: string, skills: string[] }) {
+function ProjectCard({ name, src, alt, skills, href }: { name: string, src: string, alt: string, skills: string[], href: string }) {
     const [hover, setHover] = useState(false);
 
     const handleHover = () => {
@@ -16,7 +16,7 @@ function ProjectCard({ name, src, alt, skills }: { name: string, src: string, al
             <div className={styles.details} onMouseLeave={handleHover} onMouseEnter={handleHover} data-hover={hover}>
                 <p className={styles.name}>{name}</p>
                 <p className={styles.skills}>{skills.map((skill) => `${skill} `)}</p>
-                <Link className={styles.link} href={'/'}>
+                <Link className={styles.link} href={href}>
                     For More Information
                 </Link>
             </div>
--- components/projectsSection/ProjectsSection.tsx
+++ components/projectsSection/ProjectsSection.tsx
@@ -10,7 +10,7 @@ function ProjectsSection() {
                 <h2>Projects</h2>
                 <p className={style.subtitle}>What I have built</p>
                 <div className={style.cards}>
-                    {projects.map((project, index) => <ProjectCard key={`${index}_${project.name}`} name={project.name} src={project.src} alt={project.alt} skills={project.skills} />)}
+                    {projects.map((project, index) => <ProjectCard key={`${index}_${project.name}`} name={project.name} src={project.src} alt={project.alt} skills={project.skills} href={project.href} />)}
                 </div>
             </div>
         </div>
