4b1db17dbeb07c19b4d1a75d489241c78aee2c07
yarinmatmoni <yarinmatmoni@gmail.com>
2022-12-14 19:24:57 +0200

    send email

--- components/contactSection/ContactSection.tsx
+++ components/contactSection/ContactSection.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Image from 'next/image';
 import { contact } from '../../utils/content/index';
 import { contactForm } from '../../utils/content/common';
+import emailjs from '@emailjs/browser';
 import style from './ContactSection.module.scss';
 
-//TODO: Send email - email.js + whatsapp + call
+//TODO: Email validation + show when the mail sent + whatsapp + call
 
 function ContactSection() {
+    const form: any = useRef();
+
+    const handleOnSubmit = (event: any) => {
+        event.preventDefault();
+
+
+        emailjs.sendForm(`${process.env.NEXT_PUBLIC_SERVICE_ID}`, `${process.env.NEXT_PUBLIC_TEMPLATE_ID}`, form.current, `${process.env.NEXT_PUBLIC_PUBLIC_KEY}`)
+            .then((result) => {
+                console.log(result.text);
+                event.target.reset();
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
+
     return (
         <div className="section" id="contact">
             <div className={style.container}>
@@ -22,16 +38,16 @@ function ContactSection() {
                         </div>
                     </div>
                     <div className={style.mail}>
-                        <form>
+                        <form method="post" ref={form} onSubmit={handleOnSubmit}>
                             {contactForm.map((item) => (
                                 <div key={item.name} className={style.formItem}>
                                     <label>{item.title}</label>
-                                    <input type={item.type} required></input>
+                                    <input type={item.type} name={item.name} required></input>
                                 </div>
                             ))}
                             <div className={style.formItem}>
                                 <label>Message</label>
-                                <textarea required></textarea>
+                                <textarea name='message' required></textarea>
                             </div>
                             <button type="submit">Send Message</button>
                         </form>
