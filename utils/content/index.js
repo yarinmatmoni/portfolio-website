7dbaad7d4f109eebd33e74b0e3fbba3d41bfc983
yarinmatmoni <yarinmatmoni@gmail.com>
2022-12-23 14:52:47 +0200

    check - new path for images

--- utils/content/index.js
+++ utils/content/index.js
@@ -3,17 +3,17 @@ export const mainSection =
 
 export const iconsMainSection = [
 	{
-		src: '/icons/linkedin_icon.svg',
+		src: 'portfolio-website/icons/linkedin_icon.svg',
 		alt: 'Linkedin',
 		href: 'https://www.linkedin.com/in/yarin-matmoni/',
 	},
 	{
-		src: '/icons/github_icon.svg',
+		src: 'portfolio-website/icons/github_icon.svg',
 		alt: 'Github',
 		href: 'https://github.com/yarinmatmoni',
 	},
 	{
-		src: '/icons/mail_icon.svg',
+		src: 'portfolio-website/icons/mail_icon.svg',
 		alt: 'Email',
 		href: 'https://mail.google.com/mail/?view=cm&fs=1&to=yarinmatmoni@gmail.com',
 	},
