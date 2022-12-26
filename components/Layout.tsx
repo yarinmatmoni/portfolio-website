ccaffeb713a9b9646ac3c6e456bb2662cc9baa4d
yarinmatmoni <yarinmatmoni@gmail.com>
2022-12-26 16:07:27 +0200

    move shortcut to Layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import Navbar from "./navbar/Navbar";
 
@@ -8,6 +9,9 @@ type Props = {
 function Layout({ children }: Props) {
     return (
         <>
+            <Head>
+                <link rel="shortcut icon" type="image/x-icon" href="/icons/developer_icon.svg" />
+            </Head>
             <Navbar />
             <div id="content">{children}</div>
         </>
