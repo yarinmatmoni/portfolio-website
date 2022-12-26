2914fb36e5515dca6cb5609abc0e3f04e1033117
yarinmatmoni <yarinmatmoni@gmail.com>
2022-12-26 16:09:53 +0200

    Revert "move shortcut to Layout component"
    This reverts commit ccaffeb713a9b9646ac3c6e456bb2662cc9baa4d.

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import React from "react";
 import Navbar from "./navbar/Navbar";
 
@@ -9,9 +8,6 @@ type Props = {
 function Layout({ children }: Props) {
     return (
         <>
-            <Head>
-                <link rel="shortcut icon" type="image/x-icon" href="/icons/developer_icon.svg" />
-            </Head>
             <Navbar />
             <div id="content">{children}</div>
         </>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.scss";
 import Layout from "../components/Layout";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <link rel="shortcut icon" type="image/x-icon" href="/icons/developer_icon.svg" />
+      </Head>
       < Component {...pageProps} />
     </Layout>
   )
