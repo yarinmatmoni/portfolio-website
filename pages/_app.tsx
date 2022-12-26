8000ca881ae2ba2dd6acc76589b1cb056b96be97
yarinmatmoni <yarinmatmoni@gmail.com>
2022-12-26 16:25:34 +0200

    shortcut icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import "../styles/globals.scss";
 import Layout from "../components/Layout";
 import type { AppProps } from "next/app";
+import logoImage from 'public/icons/developer_icon.svg';
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Head>
-        <link rel="shortcut icon" type="image/x-icon" href="/icons/developer_icon.svg" />
+        <link rel="shortcut icon" type="image/x-icon" href={logoImage.src} />
       </Head>
       < Component {...pageProps} />
     </Layout>
